d23db3646b3d8a7b59b673fe158da11314597bdc
jgyIBG <infoicho@gmail.com>
2025-07-31 01:37:24 -0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,45 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Navbar from "@/components/layout/navbar";
+import Footer from "@/components/layout/footer";
+import Home from "@/pages/home";
+import About from "@/pages/about";
+import Projects from "@/pages/projects";
+import Writing from "@/pages/writing";
+import Contact from "@/pages/contact";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/writing" component={Writing} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
